470e85ec32768b9fa00e250acb5256e0f5d38be1
statisticsguru1 <30623588+statisticsguru1@users.noreply.github.com>
2022-08-21 10:05:44 +0300

    Add files via upload

--- macro_evaluation.R
+++ macro_evaluation.R
@@ -27,6 +27,32 @@ mymetrics=function(actual,predicted){
   macros=data.frame(macroPrecision, macroRecall, macroF1)
   
   liss=list(accuracy,perclass,macros)
-  names(liss)<c("Accuracy","Per class metrics","Macro averaged metrics")
+  names(liss)<-c("Accuracy","Per class metrics","Macro averaged metrics")
   liss
+}
+
+
+
+mysummaryFun=function(data, lev = NULL, model = NULL){
+  # confusion metric
+  cm = as.matrix(table(Actual = data$obs, Predicted = - data$pred))
+  # Basic variables
+  n = sum(cm) # number of instances
+  nc = nrow(cm) # number of classes
+  diag = diag(cm) # number of correctly classified instances per class 
+  rowsums = apply(cm, 1, sum) # number of instances per class
+  colsums = apply(cm, 2, sum) # number of predictions per class
+  p = rowsums / n # distribution of instances over the actual classes
+  q = colsums / n # distribution of instances over the predicted classes
+  
+  # per class metrics
+  precision = diag / colsums 
+  recall = diag / rowsums 
+  f1 = 2 * precision * recall / (precision + recall) 
+
+  # Macro averaged
+  
+  out=mean(f1)
+  names(out) <- "Macro_F1_score"
+  out
 }
